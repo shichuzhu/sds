6fc9bcf350749f13db614a1261d8a0a76ec18684
Shichu Zhu <shichuzhu@gmail.com>
2018-09-14 03:24:26 -0500

    Add simple ssh supporting for VMs

---
+++ vmsetup/README.md
@@ -0,0 +1,9 @@
+# Setting up the vms
+
+## To setup the vm for the first time
+
+1. Create ppk pair with `ssh-keygen`
+2. Tune user (netid) and group number info in `deploy` file.
+3. Deploy the public keys to vms. `./deploy CopyKey`
+   - Need to "yes" and "passwd"
+4. For each vm, ssh and run the setup script. `./deploy For 2 Each "bash -s" '< setup.sh'`
---
+++ vmsetup/setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Run using
+# ssh szhu28@fa18-cs425-g44-01.cs.illinois.edu "bash -s" < setup.sh
+# For bulk run, use 
+# Sadly, the input will be directed to only the first instance.
+# Therefore, need to use subprocess.Popen, with shell=True, and quote the '< setup.sh'
+# ./deploy For 2 Each "bash -s" '< setup.sh'
+
+mkdir go
+echo 'export PATH=$PATH:$(go env GOPATH)/bin' >> .bashrc
+go get google.golang.org/grpc
+go get github.com/golang/protobuf/protoc-gen-go
