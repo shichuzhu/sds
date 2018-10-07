6756391ae6df318dd15e83319e1269b428a941da
Shichu Zhu <shichuzhu@gmail.com>
2018-10-07 14:46:38 -0500

    Sender skeleton.

--- src/lib/membership/sender.go
+++ src/lib/membership/sender.go
@@ -1,14 +1,54 @@
 package membership
 
+import (
+	"sync"
+	"time"
+)
+
+type AckWaitEntry struct {
+	addr  string
+	acked bool
+	lock  sync.Mutex
+}
+
+func (s *AckWaitEntry) ack() {
+	s.lock.Lock()
+	s.acked = true
+	s.lock.Unlock()
+}
+
+func (s *AckWaitEntry) reset() {
+	s.lock.Lock()
+	s.acked = false
+	s.lock.Unlock()
+}
+
 func senderService() error {
-	memsToPing := MembershipList.getPingTargets(3)
+	ackWaitEntries := make([]AckWaitEntry, NodeNumberToPing)
 	for {
+		memsToPing := MembershipList.getPingTargets(NodeNumberToPing)
+		for i, addr := range memsToPing {
+			ackWaitEntries[i] = AckWaitEntry{addr: addr}
+		}
 		for i := 0; i < 3; i++ { // Send 3 times.
-			for _, addr := range memsToPing {
-				Xmtr.WriteTo([]byte{}, addr) // TODO: send marshall ping message
+			for j, addr := range memsToPing {
+				UdpSendSingle(addr, []byte{}) // TODO: add message here.
 			}
 		}
-		//time.Sleep()
+		time.Sleep(1500 * time.Millisecond)
+		for i := range ackWaitEntries {
+			if ackWaitEntries[i].acked == true {
+				continue
+			} else {
+				// TODO: Form fail message for ackW[i].addr
+				message := []byte{}
+				for _, addrs := range MembershipList.getRandomTargets(len(MembershipList.members)) {
+					UdpSend(addrs, message, 2)
+				}
+				MembershipList.deleteID(ackWaitEntries[i].addr, int(^uint(0)>>1))
+			}
+		}
+
 	}
 	return nil
 }
---
+++ src/lib/membership/transmitter.go
@@ -0,0 +1,50 @@
+package membership
+
+import (
+	"net"
+	"time"
+)
+
+type NetworkStatsType struct {
+	bytesCount int
+	startTime  time.Time
+	endTime    time.Time
+}
+
+var networkStats NetworkStatsType
+
+func (s *NetworkStatsType) initNetworkStats() {
+	s.startTime = time.Now()
+	s.bytesCount = 0
+}
+
+func (s *NetworkStatsType) getBandwidthUsage() float32 {
+	s.endTime = time.Now()
+	durationInSecond := s.endTime.Sub(s.startTime).Seconds()
+	return float32(s.bytesCount) / float32(durationInSecond)
+}
+
+func UdpSend(IP string, buf []byte, rep int) {
+	addr, _ := net.ResolveUDPAddr("udp", IP)
+	conn, _ := net.DialUDP("udp", nil, addr)
+	for i := 0; i < rep; i++ {
+		_, err := conn.WriteToUDP(buf, addr)
+		Err_handler(err)
+		networkStats.bytesCount += len(buf)
+	}
+	conn.Close()
+}
+
+func UdpSendSingle(IP string, buf []byte) {
+	addr, _ := net.ResolveUDPAddr("udp", IP)
+	conn, _ := net.DialUDP("udp", nil, addr)
+	_, err := conn.WriteToUDP(buf, addr)
+	Err_handler(err)
+	networkStats.bytesCount += len(buf)
+	conn.Close()
+}
+
+// TODO: implement this when the message is done.
+//func UdpRecvSingle() pb.Message, error  {
+//	return nil, nil
+//}
