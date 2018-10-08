a708d5bbf18f76eb94caef1af55a7b09d4930b96
Shichu Zhu <shichuzhu@gmail.com>
2018-10-07 23:07:03 -0500

    Bug fixed, ready to deploy to VMs

--- src/dswim/dswim.go
+++ src/dswim/dswim.go
@@ -18,17 +18,12 @@ func main() {
 	ms.PacketDrop.SetDropRate(float32(*drop))
 	if !(*introFlag) {
 		ms.MembershipList.MyPort = *port
-		//ms.ContactIntroducer("10.195.35.3:11000")
 	} else {
 		ms.MembershipList.MyPort = *port
 		ms.InitInstance()
 		ms.StartFailureDetector()
 	}
 	for {
-		//ms.DumpTable()
-		//time.Sleep(time.Duration(2000) * time.Millisecond)
-		//fmt.Println(time.Now())
-		//fmt.Printf("Average usage is %v \n", ms.NetworkStats.GetBandwidthUsage())
 		reader := bufio.NewReader(os.Stdin)
 		text, _ := reader.ReadString('\n')
 		text = strings.Replace(text, "\n", "", -1)
@@ -40,7 +35,6 @@ func main() {
 		default:
 			if len(text) >= 4 && text[:4] == "join" {
 				introAddr := text[5:]
-				//ms.JoinByIntroducer("128.174.245.229:11000")
 				ms.JoinByIntroducer(introAddr)
 			} else {
 				fmt.Println("Invalid input.")
