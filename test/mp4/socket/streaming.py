da6499b6ae15981ef81835fea209a29d6070da0d
Shichu Zhu <shichuzhu@gmail.com>
2018-12-03 14:18:20 -0600

    fix: slower input using sleep for failure demo

--- test/mp4/socket/streaming.py
+++ test/mp4/socket/streaming.py
@@ -6,15 +6,17 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: network_wordcount.py <hostname> <port>", file=sys.stderr)
+    if len(sys.argv) != 2:
+        print("USSEAGE input file path", file=sys.stderr)
         sys.exit(-1)
     sc = SparkContext(appName="PythonStreamingNetworkStreamProcessing")
     ssc = StreamingContext(sc, 1)
 
-    lines = ssc.socketTextStream(sys.argv[1], int(sys.argv[2]))
-    lines.filter(lambda line: (len(line) > 15)).map(lambda line: line.upper()).filter(
-        lambda line: ("ABCd" in line or "BCDe" in line)).saveAsTextFiles("test_folder")
+    # lines = ssc.textFileStream(sys.argv[1])
+    lines = ssc.textFileStream("test/mp4/data/input.txt")
+    result = lines.filter(lambda line: (len(line) > 15)).map(
+        lambda line: line.upper()).map(lambda line: line + "!!!")
+    print("1234567890", result.collect())
 
     ssc.start()
     ssc.awaitTermination()
